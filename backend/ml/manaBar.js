7711db61f9d9ccb719a0f565c65128b5b88eedf8
Haruki <aratashiharuki@gmail.com>
2024-06-25 05:17:46 +0300

    Fixed manabar finding

--- backend/ml/manaBar.js
+++ backend/ml/manaBar.js
@@ -4,9 +4,9 @@ const screenshot = require('screenshot-desktop')
 const manaCountour = (buffer) => {
     const img = cv.imdecode(buffer)
 
-    const optimalImage = img.cvtColor(cv.COLOR_HSV2BGR).gaussianBlur(new cv.Size(5, 5), 1)
-    const lowerBlue = new cv.Vec(100, 80, 0);
-    const upperBlue = new cv.Vec(150, 150, 100);
+    const optimalImage = img.cvtColor(cv.COLOR_HSV2BGR).gaussianBlur(new cv.Size(1, 1), 1)
+    const lowerBlue = new cv.Vec(160, 50, 50);
+    const upperBlue = new cv.Vec(250, 255, 255);
     const mask = optimalImage.inRange(lowerBlue, upperBlue);
 
     const kernel = new cv.Mat(3, 3, cv.CV_8U, 1);
@@ -16,16 +16,13 @@ const manaCountour = (buffer) => {
     const closed = dilated.morphologyEx(kernel, cv.MORPH_CLOSE);
 
     const contours = closed.findContours(cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
-    // cv.imshow('Test', closed)
-    // cv.waitKey(0)
     
     return contours.filter(contour => {
         const rect = contour.boundingRect()
         const aspectRatio = rect.width / rect.height
         const square = rect.width * rect.height
 
-        return (aspectRatio <= .3 && square > 5000)
+        return (aspectRatio <= .3 && square > 2500 && rect.height < closed.rows / 2)
     }).sort((a, b) => {
         const aRect = a.boundingRect()
         const bRect = b.boundingRect()
